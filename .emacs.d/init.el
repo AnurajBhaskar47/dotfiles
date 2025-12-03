09fb24ceb8547c22e6969bbc2bc180a6fb35f54f
AnurajBhaskar47 <bhaskar25903@gmail.com>
2025-12-03 11:10:30 +0530

    added themes and removed backup files in .emacs.d

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,13 +1,20 @@
+(setq make-backup-files nil)
 (menu-bar-mode 0)
 (tool-bar-mode 0)
 (column-number-mode 1)
 (global-display-line-numbers-mode)
+;;; -*- lexical-binding: t -*-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(display-line-numbers-type 'relative))
+ '(custom-enabled-themes '(gruber-darker))
+ '(custom-safe-themes
+   '("e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7"
+     default))
+ '(display-line-numbers-type 'relative)
+ '(package-selected-packages '(gruber-darker-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
